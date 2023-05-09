39b41d771142354edc83987ebfb4b4f0cb0116fd
Alisa <alisa.luoma@hotmail.fi>
2023-05-09 12:54:15 +0300

    Doing coutry challenge

---
+++ osa2/tehtavat_18-20/src/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import coutriesService from "./servisces/coutries";
+import ShowData from './components/ShowData.jsx'
+
+const App =()=> {
+  const [countries, setCountries] = useState([])
+  const [lookCountry, setLookCountry] = useState('')
+  const [showData, setShowData] = useState(false)
+  const [message, setMessage] = useState(null)
+
+  const handleCountryChange =(e)=>{
+    setLookCountry(e.target.value)
+
+     console.log(e.target.value.length,  " " , e.target.value !== 0)
+
+    coutriesService
+      .getAll()
+      .then(responseCountries => {
+        const foundCountries = responseCountries.filter(country => country.name.common.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase()))
+        if (foundCountries.length === 0) {
+          setMessage('Too low matches, specify another filter')
+          setCountries([]);
+        } else if(foundCountries.length < 11) {
+          setMessage(null)
+          setCountries(foundCountries);
+        } else {
+          setCountries([]);
+          if(e.target.value.length === 0){
+            setMessage(null)
+          } else {
+            setMessage('Too many matches, specify another filter')
+          }
+        }
+      })
+    }
+  
+
+  const handleShowSelectedCountry = (country) => {
+    setCountries([country])
+  }
+
+  return (
+    <div>
+      <p>Find countries <input value={lookCountry} onChange={handleCountryChange}></input> </p>
+      <ShowData countriesData={countries} handleShowSelectedCountry={handleShowSelectedCountry} message={message}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ osa2/tehtavat_18-20/src/components/ShowData.jsx
@@ -0,0 +1,49 @@
+const ShowData =({countriesData, handleShowSelectedCountry, message}) => {
+    if(message !== null && countriesData.length === 0){
+        return (
+            <div>
+                <p>{message}</p>
+            </div>
+        )
+    }
+    if(message === null && countriesData.length === 0){
+        return (
+            <div>
+            </div>
+        )
+    }
+
+    if(countriesData.length === 1){
+        // console.log(countriesData[0].languages)
+        // Object.entries(countriesData[0].languages).map(([key, value]) =>  console.log(`${key}: ${value}`))
+        return (
+            <div>
+                <h2>{countriesData[0].name.common}</h2>
+                <p>capital {countriesData[0].capital}</p>
+                <p>area {countriesData[0].area}</p>
+                <h4>Languages:</h4>
+                <ul>
+                {
+                    Object.entries(countriesData[0].languages).map(([key, value]) => {return(<li key={value}>{value}</li>)})
+                }
+                </ul>
+                <img style={style} src={countriesData[0].flags.png} alt={countriesData[0].flags.alt}/>
+            </div>
+        )
+    }
+    return (
+        <div>
+            {
+                countriesData.map((country, id)=>
+                <p key={id}>{country.name.common}<button onClick={()=>handleShowSelectedCountry(country)}>show</button></p>
+                )
+            }
+        </div>
+    )
+}
+
+export default ShowData
+
+const style = {
+    height: "200px"
+}
